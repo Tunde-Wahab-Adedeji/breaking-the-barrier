edc08e2d73be7eed77839f020a0de94e662d5116
Tunde-Wahab-Adedeji <76218216+Tunde-Wahab-Adedeji@users.noreply.github.com>
2021-02-06 13:56:11 +0100

    Add files via upload

---
+++ lastesttimefor.py
@@ -0,0 +1,25 @@
+import re,time
+c=time.asctime()   
+a,d=(int,re.findall(r'\d+',c))
+u=(c[4:7])
+l=(c[0:3])
+print(l)
+#print(c)
+p=('%s%s%s%s%s' %(d[0],'/',u,'/',d[4]))
+print(p)
+h=('%s%s%s%s%s'%(d[1],':',d[2],':',d[3]   ))
+if(int(d[1])>7 or int(d[2])>30):
+	g=int(d[1])-7
+	k=int(d[2])-30
+	print('you are late to school by %d %s%s %d %s'%(g,'hrs','|',k,'min'))
+else:
+	print('you are early to school')
+if(int(d[1])<=11):
+	go='AM'
+	print(h+go)
+else:
+	go='PM'
+	print(h+go)
+	
+	
+
